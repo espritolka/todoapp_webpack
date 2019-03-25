6655b4a83964d0c1c09eb60fb5f81acf3e2b060f
Olga <espritolka@gmail.com>
2019-03-25 03:29:01 -0400

    first commit

---
+++ src/deleteTasc.js
@@ -0,0 +1,8 @@
+const deleteTask = function(e) {
+  console.log("Delete Task...", e);
+  //Remove the parent list item from the ul
+  var listItem = e.target.parentNode;
+  var ul = listItem.parentNode;
+ul.removeChild(listItem);
+};
+export default deleteTask;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import addTask from './addTask';
+const addTaskButton = document.getElementById("addTask");
+addTaskButton.addEventListener("click", addTask);
